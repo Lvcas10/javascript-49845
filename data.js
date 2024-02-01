4c8453bf268360064c4bc392d5b2bc82d253f72e
Lucas Perez <lucaszagustin@gmail.com>
2024-02-01 17:04:50 -0300

    Tercera Pre Entrega

---
+++ data.js
@@ -0,0 +1,98 @@
+const products = [
+    {
+        "id": 1,
+        "name": "Lenovo Thinkpad",
+        "description": "Ryzen 7 8gb Ram",
+        "category": "Computacion",
+        "price": 450,
+        "img": "./assets/lenovo.jpeg"
+    },
+    {
+        "id": 2,
+        "name": "Parlante Kassel",
+        "description": "PS082 Bluetooh",
+        "category": "Perisfericos",
+        "price": 110,
+        "img": "./assets/parlante.jpeg"
+    },
+    {
+        "id": 3,
+        "name": "Teclado Gamer",
+        "description": "Redragon K530W-RGB-PRO",
+        "category": "Perisfericos",
+        "price": 85,
+        "img": "./assets/teclado.jpeg"
+    },
+    {
+        "id": 4,
+        "name": "Disco Solido Kingston",
+        "description": "240Gb Kingston A400",
+        "category": "Componentes",
+        "price": 180,
+        "img": "./assets/disco.jpeg"
+    },
+    {
+        "id": 5,
+        "name": "Monitor Gamer 49''",
+        "description": "HRD 144Hz 4Ms XG49VQ",
+        "category": "Computacion",
+        "price": 250,
+        "img": "./assets/monitor.jpeg"
+    },
+    {
+        "id": 6,
+        "name": "Mouse Gamer",
+        "description": "M808W-KSA Storm Pro",
+        "category": "Perisfericos",
+        "price": 45,
+        "img": "./assets/mouse.jpeg"
+    },
+    {
+        "id": 7,
+        "name": "Jostick PC",
+        "description": "Redragon G807 Saturno",
+        "category": "Perisfericos",
+        "price": 90,
+        "img": "./assets/jostick.jpeg"
+    },
+    {
+        "id": 8,
+        "name": "Placa de Video",
+        "description": "RTX 4090 24Gb Msi Suprim X",
+        "category": "Componentes",
+        "price": 270,
+        "img": "./assets/placa.jpeg"
+    },
+    {
+        "id": 9,
+        "name": "Memoria Ram",
+        "description": "8Gb 3600 Mhz",
+        "category": "Componentes",
+        "price": 160,
+        "img": "./assets/ram.jpeg"
+    },
+    {
+        "id": 10,
+        "name": "Teclado Gamer Logitech",
+        "description": "G Pro LOL2 Mecanico",
+        "category": "perisfericos",
+        "price": 60,
+        "img": "./assets/teclado2.jpeg"
+    },
+    {
+        "id": 11,
+        "name": "Notebook Samsung",
+        "description": "Pro Core i5 512Gb 16Gb",
+        "category": "Computacion",
+        "price": 650,
+        "img": "./assets/samsung.jpeg"
+    },
+    {
+        "id": 12,
+        "name": "Notebook Dell",
+        "description": "Pro i7 32Gb Ssd 512Gb 4Gb 16",
+        "category": "Computacion",
+        "price": 750,
+        "img": "./assets/dell.jpeg"
+    }
+]
---
+++ index.js
@@ -0,0 +1,117 @@
+const modal = new bootstrap.Modal('#modalCarrito', {});
+const btnModalCarrito = document.querySelector('#btnModalCarrito');
+const cartCount = document.querySelector('#cartCount');
+const cartSum = document.querySelector('#cartSum');
+const inputSearch = document.querySelector('#inputSearch');
+const listProducts = document.querySelector('#listProducts');
+const selectCategory = document.querySelector('#selectCategory');
+const modalListProducts = document.querySelector('#modalListProducts');
+const btnClose = document.querySelector('#btnClose');
+const btnSave = document.querySelector('#btnSave');
+const btnOrder = document.querySelector('#btnOrder');
+const btnOrderUp = document.querySelector('#btnOrderUp');
+
+const listCart = JSON.parse( localStorage.getItem('cart') ) || [];
+const cart = new Cart(listCart);
+
+cartCount.innerText = cart.getCount();
+
+btnModalCarrito.addEventListener('click', function(){
+    const list = cart.getProducts();
+    redenCart(list);
+    cartSum.innerText = cart.getSum();
+    modal.show();
+})
+
+btnClose.addEventListener('click', () => {
+    modal.hide();
+})
+
+btnOrder.addEventListener('click', ()=> {
+    products.sort(  (a, b ) => {
+        if(  a.price < b.price  ){
+            return -1
+        }
+        if ( a.price > b.price){
+            return 1
+        }
+
+        return 0
+    } )
+
+    renderProducts(products);
+})
+
+btnOrderUp.addEventListener('click', ()=> {
+    products.sort(  (a, b ) => {
+        if ( a.price > b.price){
+            return -1
+        }
+        if(  a.price < b.price  ){
+            return 1
+        }
+
+        return 0
+    } )
+
+    renderProducts(products);
+})
+
+selectCategory.addEventListener('click', (event) => {
+    const search = selectCategory.value
+    const newList = products.filter(  (product) => product.category.toLowerCase().includes( search.toLowerCase() ) );
+    renderProducts(newList);
+})
+
+inputSearch.addEventListener('input', (event) => {
+    const search = inputSearch.value
+    const newList = products.filter(  (product) => product.name.toLowerCase().includes( search.toLowerCase() ) );
+    renderProducts(newList);
+})
+
+const renderProducts = (list) => {
+    listProducts.innerHTML = '';
+    list.forEach(product => {
+        listProducts.innerHTML +=
+            `<div class="col-sm-4 col-md-3">
+                <div class="card p-2">
+                    <h4>${product.name} </h4>
+                    <img class="img-fluid" src="${product.img}" alt="${product.name}">
+                    <p class="text-center">${product.category}
+                    <h3 class="text-center">$${product.price} </h3>
+                    <button id="${product.id} " type="button" class="btn btn-primary btnAddCart">
+                        <i class="fa-solid fa-cart-plus"></i> Agregar
+                    </button>
+                </div>
+            </div>`;
+    });
+
+
+    const btns = document.querySelectorAll(".btnAddCart")
+    btns.forEach (btn => {
+        btn.addEventListener("click", addToCart);
+    })
+}
+
+const addToCart = (evento) => {
+    const id = evento.target.id;
+    const product = products.find (item => item.id == id );
+    console.log(product);
+    cart.addToCart(product);
+    cartCount.innerText = cart.getCount();
+}
+
+const redenCart = (list) => {
+    modalListProducts.innerHTML = '';
+    list.forEach( product => {
+        modalListProducts.innerHTML += // html
+            `<tr>
+                <td> ${product.name} </td>
+                <td> ${product.units}</td>
+                <td>$${product.price}</td>
+                <td>$${product.price * product.units}</td>
+            </tr>`
+    });
+}
+
+renderProducts( products);
